3f4c56bd814dddf9ee0690424acd9e32337262aa
DevBidon <dev@bidon-gs.com>
2018-06-14 00:20:42 +0300

    Added sorting and player scripts

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,15 @@
+﻿public class Player
+{
+    public string Name { get; set; }
+    public int Coins { get; private set; }
+    public int Points { get; private set; }
+    public int Health { get; private set; }
+
+    public Player(string name, int coins, int points, int health)
+    {
+        Name = name;
+        Coins = coins;
+        Points = points;
+        Health = health;
+    }
+}
---
+++ Assets/Scripts/SortScript.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using UnityEngine;
+
+public class SortScript : MonoBehaviour
+{
+    private void Start()
+    {
+        Player[] players = {
+            new Player("Johan", 100, 200, 300), 
+            new Player("Alex", 150, 250, 320),
+            new Player("James", 250, 375, 150)
+        };
+        
+        Predicate<Player> predicate = FindPlayers;
+
+        var foundedPlayers = Array.FindAll(players, predicate);
+        
+        OutputFoundedPlayer(foundedPlayers);
+    }
+
+    private static bool FindPlayers(Player obj)
+    {
+        return obj.Coins >= 150;
+    }
+
+    private void OutputFoundedPlayer(Player[] players)
+    {
+        foreach (var player in players)
+        {
+            print(player.Name + " has " + player.Coins + " coins!");
+        }
+    }
+}
